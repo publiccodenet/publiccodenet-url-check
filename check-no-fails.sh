408f81293e1954f6e389f2c05e8e895d1d5b0544
Eric Herman <eric@freesa.org>
2023-05-10 12:20:51 +0200

    need docopt, exit on error
    Co-authored-by: Jan Ainali <jan@publiccode.net>

--- check-no-fails.sh
+++ check-no-fails.sh
@@ -2,6 +2,10 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2023 The Foundation for Public Code <info@publiccode.net>
 
+# $ help set | grep '\-e'
+#      -e  Exit immediately if a command exits with a non-zero status.
+set -e
+
 url-check/url-check.py
 LINES=`wc -l url-check-fails.json | cut -f1 -d' '`
 if [ $LINES -gt 1 ]; then
